1d63f1720d48dff2f36d351790494cb11db529e7
mandariny <mandariny716@gmail.com>
2022-05-22 01:38:03 +0900

    Design: 중간 결과 출력창 추기
    중간 결과 출력창 추가
    
    Related to: #1

--- src/calculator/Caculator.java
+++ src/calculator/Caculator.java
@@ -7,8 +7,10 @@ public class Caculator extends JFrame{
 	private static Color gray = new Color(240, 240, 240);
 	private static Color teal = new Color(74, 170, 169);
 	private static Color deepGray = new Color(94, 94, 94);
+	private static Color lightGray = new Color(200, 200, 200);
 	private static Color orange = new Color(252, 165, 60);
 	private static Font defaultFont = new Font("Arial", Font.BOLD, 30);
+	private static Font answerFont = new Font("Arial", Font.PLAIN, 18);
 	
 	public Caculator() {
 		setTitle("계산기");
@@ -18,15 +20,15 @@ public class Caculator extends JFrame{
 		contentPane.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		
-		JTextArea text = new JTextArea(3, 0);
-		text.setSize(400, 300);
+		// 계산식 입력란
+		JTextArea text = new JTextArea("1234+123141");
 		text.setBackground(gray);
 		text.setFont(defaultFont);
 		text.setForeground(deepGray);
 		text.setLineWrap(true);
 		text.setRows(3);
-		text.setMaximumSize(new Dimension(4, 2));
-		text.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		text.setAlignmentY(RIGHT_ALIGNMENT);
+		text.setEditable(false);
 
 		// (0,0)칸에 가중치 1:1로 삽입
 		gbc.fill = GridBagConstraints.BOTH;
@@ -34,16 +36,35 @@ public class Caculator extends JFrame{
 		gbc.weighty = 1.0;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		gbc.insets = new Insets(10, 10, 10, 10);
 		
 		contentPane.add(text, gbc);
 		
+		// 중간 답 출력란
+		JTextField answer = new JTextField("answer");
+		answer.setBackground(gray);
+		answer.setFont(answerFont);
+		answer.setForeground(lightGray);
+		answer.setBorder(null);
+		answer.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		answer.setEditable(false);
+		
+		// (1,0)칸에 가중치 1:0.2로 삽입
+		gbc.weightx = 1.0;
+		gbc.weighty = 0.2;
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.insets = new Insets(0, 0, 0, 10);
+		
+		contentPane.add(answer, gbc);
+		
+		// 넘버 패드 
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new GridLayout(5, 4, 10, 10));
 		buttonPanel.setBackground(gray);
 		buttonPanel.setSize(400, 500);
 		
-		// 넘버 패드 
-		String[] buttonLabels = {"C", "(", ")", "%", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="};
+		String[] buttonLabels = {"C", "(", ")", "/", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="};
 		int len = buttonLabels.length;
 		JButton[] buttons = new JButton[len];
 		
@@ -64,11 +85,12 @@ public class Caculator extends JFrame{
 			buttonPanel.add(buttons[i]);
 		}
 		
-		// (1,0)칸에 가중치 1:1로 삽입
+		// (2,0)칸에 가중치 1:1로 삽입
 		gbc.weightx = 1.0;
 		gbc.weighty = 1.0;
 		gbc.gridx = 0;
-		gbc.gridy = 1;
+		gbc.gridy = 2;
+		gbc.insets = new Insets(0, 10, 10, 10);
 		
 		contentPane.add(buttonPanel, gbc);
 		
