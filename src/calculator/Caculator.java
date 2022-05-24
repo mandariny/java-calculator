4f9d541d68157fc81076f524a0e8422abdf60ea7
mandariny <mandariny716@gmail.com>
2022-05-24 23:26:34 +0900

    Feat: EventListener 구현
    Mouse, Key EventListener 구현
    *(곱하기) 연산자 입력시 논리적 오류 발생
    
    Related to: #3

--- src/calculator/Caculator.java
+++ src/calculator/Caculator.java
@@ -2,26 +2,36 @@ package calculator;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class Caculator extends JFrame{
+	// 글자 및 컴포넌트에 사용하는 색상
 	private static Color gray = new Color(240, 240, 240);
 	private static Color teal = new Color(74, 170, 169);
 	private static Color deepGray = new Color(94, 94, 94);
 	private static Color lightGray = new Color(200, 200, 200);
 	private static Color orange = new Color(252, 165, 60);
+	
+	// 주로 사용하는 폰트 설정
 	private static Font defaultFont = new Font("Arial", Font.BOLD, 30);
 	private static Font answerFont = new Font("Arial", Font.PLAIN, 18);
 	
-	public Caculator() {
+	// 내부 클래스에서 접근할 수 있는 컴포넌트
+	private static Container contentPane;
+	private static JTextArea text;
+	private static JTextField answer;
+	
+	private Caculator() {
 		setTitle("계산기");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		Container contentPane = getContentPane();
+		contentPane = getContentPane();
 		contentPane.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
+		contentPane.addKeyListener(new MyKeyListener());
 		
 		// 계산식 입력란
-		JTextArea text = new JTextArea("1234+123141");
+		text = new JTextArea();
 		text.setBackground(gray);
 		text.setFont(defaultFont);
 		text.setForeground(deepGray);
@@ -41,7 +51,7 @@ public class Caculator extends JFrame{
 		contentPane.add(text, gbc);
 		
 		// 중간 답 출력란
-		JTextField answer = new JTextField("answer");
+		answer = new JTextField("answer");
 		answer.setBackground(gray);
 		answer.setFont(answerFont);
 		answer.setForeground(lightGray);
@@ -64,7 +74,7 @@ public class Caculator extends JFrame{
 		buttonPanel.setBackground(gray);
 		buttonPanel.setSize(400, 500);
 		
-		String[] buttonLabels = {"C", "(", ")", "/", "7", "8", "9", "x", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="};
+		String[] buttonLabels = {"C", "(", ")", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "<-", "0", ".", "="};
 		int len = buttonLabels.length;
 		JButton[] buttons = new JButton[len];
 		
@@ -74,6 +84,7 @@ public class Caculator extends JFrame{
 			buttons[i].setFont(defaultFont);
 			buttons[i].setForeground(deepGray);
 			buttons[i].setBorderPainted(false);
+			buttons[i].addActionListener(new MyActionListener());
 			
 			if(i == len - 1) {
 				buttons[i].setBackground(teal);
@@ -97,6 +108,108 @@ public class Caculator extends JFrame{
 		setSize(400, 700);
 		setLocation(700, 200);
 		setVisible(true);
+		
+		contentPane.setFocusable(true);
+		contentPane.requestFocus();
+	}
+	
+	// 마지막 글자 검사
+	private int checkInputString(String msg, char btn) {
+		// 부호가 입력된 경우
+		if(btn == '+' || btn == '-' || btn == '/' || btn == '*') {
+			// 첫 문자가 부호인 경우
+			if(msg.length() == 0) 
+				return -1;
+			
+			char last = msg.charAt(msg.length() - 1);
+
+			// 연속으로 부호가 입력되는 경우
+			if(last == '+' || last == '-' || last == '/' || btn == '*') {
+				// 부호가 같은 경우
+				if(last == btn) 
+					return -1;
+				
+				// 부호가 다른 경우
+				return 0;
+			}
+		}
+		
+		return 1;
+	}
+	
+	private class MyActionListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JButton btn = (JButton)e.getSource();
+			String pressedBtn = btn.getText();
+			
+			String msg = text.getText();
+			
+			// 0 : 부호만 바뀜. 추가되지 않음
+			// 1 : 입력된 텍스트 추가
+			// -1 : 텍스트에 변화 없음
+			int input = checkInputString(msg, pressedBtn.charAt(0));
+			
+			if(input == 0) {
+				text.setText(msg.substring(0, msg.length() - 1) + pressedBtn);
+			}else if(input == 1) {
+				if(pressedBtn == "=") {
+					// 계산 요청
+				}else if(pressedBtn == "C"){ //전체 삭제
+					text.setText("");
+				}else if(pressedBtn == "<-") { //마지막 문자 삭제
+					msg = text.getText();
+					int len = msg.length();
+					
+					// IndexOutOfBoundsException 방지
+					if(len > 0)
+						text.setText(msg.substring(0, len - 1));
+					
+				}else {
+					msg = text.getText();
+					text.setText(msg + pressedBtn);
+				}
+			}
+			
+			// 버튼에 맞춰진 focus를 contentPane으로 돌리기
+			// 마우스 클릭과 키 입력 자유롭게 이동 가능
+			contentPane.requestFocus();
+		}
+	}
+	
+	private class MyKeyListener extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			char keyChar = e.getKeyChar();
+			int keyCode = e.getKeyCode();
+			
+			String msg = text.getText();
+			
+			// 0 : 부호만 바뀜. 추가되지 않음
+			// 1 : 입력된 텍스트 추가
+			// -1 : 텍스트에 변화 없음
+			int input = checkInputString(msg, keyChar);
+			
+			if(input == 0) {
+					text.setText(msg.substring(0, msg.length() - 1) + keyChar);
+			}else if(input == 1) {
+				// 키 입력에 따른 처리 
+				// 8 = backspace, 10 = enter
+				if((keyChar >= '0' && keyChar <='9') || keyChar == '+' || keyChar == '-' || keyChar == '*' || keyChar == '/' || keyChar == '(' || keyChar == ')') {
+					msg = text.getText() + keyChar;
+					text.setText(msg);
+				}else if(keyCode == 8){
+					msg = text.getText();
+					int len = msg.length();
+					
+					// IndexOutOfBoundsException 방지
+					if(len > 0)
+						text.setText(msg.substring(0, len - 1));
+				}else if(keyCode == 10) {
+					// 계산 요청
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
